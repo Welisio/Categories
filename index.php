bf0fa8c5095536a2cd48b0636fd1cdb014915f58
Welisio <63056611+Welisio@users.noreply.github.com>
2023-05-18 12:21:00 +0400

    Init

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Categories</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <div class="space"></div>
+  <div class="category-body">
+    <div class="categories-wrapper">
+      <div class="head-category">
+        <img style="width:22px;max-height:30px" src="https://kontakt.az/wp-content/uploads/2022/10/Telefon.png">
+        <div>Smartfonlar</div> 
+      </div>
+      <div class="head-delimiter"></div>
+      <div class="head-category">
+        <img style="width:22px;max-height:30px" src="https://kontakt.az/wp-content/uploads/2022/10/Saat.png">
+        <div>Smart qadjetlər</div>
+      </div>
+      <div class="head-delimiter"></div>
+      <div class="head-category">
+        <img style="width:22px;max-height:30px" src="https://kontakt.az/wp-content/uploads/2022/10/Komputer.png">
+        <div>Notbuklar, PK, planşetlər</div>
+      </div>
+    </div>
+    <div class="categories-tree">
+      <div class="category-block">
+        <div class="main-category">Smartfonlar</div>
+        <div class="sub-categories">
+        </div>
+      </div>
+      <div class="category-block">
+        <div class="sub-category">Apple</div>
+        <div class="sub-categories">
+          <div class="sub-sub-category">iphone 14 pro Max</div>
+          <div class="sub-sub-category">iphone 14 pro Max</div>
+          <div class="sub-sub-category">iphone 14 pro Max</div>
+          <div class="sub-sub-category">iphone 14 pro Max</div>
+          <div class="sub-sub-category">iphone 14 pro Max</div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+</body>
+</html>
+
+
+SQL Strings<br>
+sub-category = 'Apple'<br>
+sub-sub-category = 'x'<br>
