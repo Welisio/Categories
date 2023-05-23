f0d2e7aded3a20210b71d73aefee68e21e6f4575
Welisio <63056611+Welisio@users.noreply.github.com>
2023-05-23 12:55:42 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+try {
+  $dsn = "mysql:host=localhost;dbname=categorydb;charset=utf8mb4";
+  $pdo = new PDO($dsn,'root','');
+  $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+  $statement = $pdo->prepare("SELECT `name`,`id` FROM `category` WHERE `type` = 0");
+
+  $statement->execute() ? $product_type0 = $statement->fetchAll(PDO::FETCH_ASSOC) : die('Something went wrong');
+
+  $productType0Ids = array();
+
+  foreach($product_type0 as $element) {
+    array_push($productType0Ids, $element['id'])
+  }
+  
+  // $statement = $pdo->prepare("SELECT `name`,`id` FROM `category` WHERE `type` = 1 AND `parent_id` IN () ");
+  // print_r($product_type0);
+  
+} catch (PDOException $e) {
+  echo 'Failed to connect to the database: ' . $e->getMessage();
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,7 +62,10 @@
       <div class="category-block">
         <div class="sub-category">Apple</div>
         <div class="sub-categories">
-          <div class="sub-sub-category">iphone 14 pro Max</div>
+          <div class="sub-sub-category">
+            iphone 14 pro Max
+            <div class="sub-sub-category" style="margin-left: 15px">Red</div>
+          </div>
           <div class="sub-sub-category">iphone 14 pro Max</div>
           <div class="sub-sub-category">iphone 14 pro Max</div>
           <div class="sub-sub-category">iphone 14 pro Max</div>
@@ -44,11 +74,10 @@
       </div>
     </div>
   </div>
- 
 </body>
 </html>
 
-
 SQL Strings<br>
+product-category = '0'<br>
 sub-category = 'Apple'<br>
-sub-sub-category = 'x'<br>
+sub-sub-category = 'iphone 14 pro|iphone 14 pro Max'<br>
