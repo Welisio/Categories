84cc26ac06122da86df954986f4abb8f3237602f
Riad Guliev <63056611+Welisio@users.noreply.github.com>
2023-06-12 11:55:08 +0400

    Add files via upload

--- categorydb.sql
+++ categorydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 25, 2023 at 04:37 AM
+-- Generation Time: Jun 12, 2023 at 07:41 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -30,7 +30,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `category` (
   `id` int UNSIGNED NOT NULL,
   `parent_id` int UNSIGNED DEFAULT NULL,
-  `nesting_type` smallint UNSIGNED NOT NULL,
   `product_type` smallint UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -39,13 +38,47 @@ CREATE TABLE `category` (
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`id`, `parent_id`, `nesting_type`, `product_type`, `name`) VALUES
-(1, NULL, 0, 0, 'Smartfonlar'),
-(2, NULL, 0, 0, 'Apple'),
-(3, 2, 1, 0, 'iphone 14 pro'),
-(4, 2, 1, 0, 'iphone 11'),
-(5, 3, 1, 0, 'white'),
-(6, 3, 1, 0, 'black');
+INSERT INTO `category` (`id`, `parent_id`, `product_type`, `name`) VALUES
+(1, NULL, 0, 'Smartfonlar'),
+(2, NULL, 0, 'Apple'),
+(3, 2, 0, 'iphone 14 pro'),
+(4, 2, 0, 'iphone 11'),
+(5, 3, 0, 'white'),
+(6, 3, 0, 'black'),
+(8, NULL, 0, 'Samsung'),
+(9, 8, 0, 'Samsung Galaxy A14'),
+(10, NULL, 1, 'Smart saatlar'),
+(11, NULL, 1, 'Apple'),
+(12, NULL, 1, 'Samsung'),
+(13, 8, 0, 'Samsung Galaxy A34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `product_type` smallint UNSIGNED NOT NULL,
+  `path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `image`, `product_type`, `path`) VALUES
+(1, 'iphone 14 pro', 'https://www.ultimateshield.co.uk/cdn/shop/products/Hdb3e1dca148f40d8a2296a360a376e384_564927c7-5f5d-423b-a896-9dad5091948c.jpg?v=1662648279', 0, '1/2/3/5'),
+(2, 'iphone 14 pro', 'https://www.nfm.com/dw/image/v2/BDFM_PRD/on/demandware.static/-/Sites-nfm-master-catalog/default/dw2311962f/images/063/17/63176101-2.jpg?sw=1000&sh=1000&sm=fit', 0, '1/2/3/6'),
+(3, 'iphone 11', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphone11-purple-2019?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1611169055000', 0, '1/2/4'),
+(4, 'Samsung Galaxy A14', 'https://image-us.samsung.com/SamsungUS/home/mobile/phones/pdp/galazy-a/galaxy-a14-5g/gallery_images/SDSAC-5314-Galaxy-A14-5G_Black_Back-R30_RGB-1600x1200_6.jpg?$product-details-jpg$', 0, '1/8/9'),
+(5, 'Apple Watch 8 45 mm Midnight Aluminium / MNP13RB/A', 'https://kontakt.az/wp-content/uploads/2022/10/Apple-Watch-8-45-mm-Midnight-Aluminium_png.webp', 1, '10/11'),
+(6, 'Apple Watch 8 41 mm Starlight Aluminium / MNP63RB/A', 'https://kontakt.az/wp-content/uploads/2022/09/Apple-Watch-8-41-mm_png.webp', 1, '10/11'),
+(7, 'Samsung Watch5 BT 44mm Blue/SM-R910NZBACIS', 'https://kontakt.az/wp-content/uploads/2022/09/New-Project-2022-09-06T102306.643_png.webp', 1, '10/12'),
+(8, 'Samsung Galaxy A34', 'https://kontakt.az/wp-content/uploads/2023/04/New-Project-2023-04-04T114228.895_png.webp', 0, '1/8/13');
 
 --
 -- Indexes for dumped tables
@@ -58,6 +91,12 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,7 +104,13 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-// Надо создать вторую таблицу,где буду храниться данные о товарах и привязать их к категориям
 try {
-  $dsn = "mysql:host=localhost;dbname=categorydb;charset=utf8mb4";
-  $pdo = new PDO($dsn,'root','');
-  $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-  $infiniteNestedArrays = array();
-  
+  require_once __DIR__ . '/core.php';
   function getCategories ($productType) {
     global $pdo;
 
@@ -28,41 +21,27 @@ try {
 
   $smartGadjets = getCategories(1);
 
-  $computers = getCategories(2);
-
-  foreach ($infiniteNestedArrays as $item) {
-    echo '<br>';
-  }
-
-  // function subCategoryRender ($categories, $category, $depth) {
-  //   if (inArray($category['id'], $categories)) {
-  //     for($index = 0; $index < count($categories); $index++) { 
-  //       if ($categories[$index]['parent_id'] === $category['id']) {
-  //         echo '<div style="margin-left: '.$depth.'px;" class="sub-sub-category">'.$categories[$index]['name'].'</div>';
-  //         subCategoryRender($categories, $categories[$index], $depth + 15);
-  //       }
-  //     }
-  //   }
-  // }
-
   function inArray ($categoryId, $categories) {
-    for ($index = 0; $index < count($categories); $index++) {
+    for ($index = 0; $index < count($categories); $index++) {     
       if ($categories[$index]['parent_id'] === $categoryId) return true;
     }
   }
 
-  function categoryRender ($categories, $depth) {
-    for ($index = 0; $index < count($categories); $index++) {
-      echo '<div style="margin-left: '.$depth.'px" class="category">'.$categories[$index]['name'].'</div>';
-      if (inArray($categories[$index]['id'], $categories)) {
-        categoryRender($categories, $depth + 15);
-      }      
-    }
+  function categoryRender ($categories, $category, $depth) {
+      if (inArray($category['id'], $categories)) {
+        for($index = 0; $index < count($categories); $index++) { 
+          if ($categories[$index]['parent_id'] === $category['id']) {
+            echo '
+            <a href="products.php?category_id='.$categories[$index]['id'].'&product_type='.$categories[$index]['product_type'].'">
+              <div style="margin-left: '.$depth.'px;" class="category">'.$categories[$index]['name'].'</div>
+            </a>
+            ';
+            categoryRender($categories, $categories[$index], $depth + 15);
+          }
+        }
+      }
   }
 
-  print_r($smartPhoneCategories);
-
-  die();
 } catch (PDOException $e) {
   echo 'Failed to connect to the database: ' . $e->getMessage();
 }
@@ -75,10 +54,10 @@ try {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Categories</title>
-  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="css/common.css">
+  <link rel="stylesheet" href="css/category.css">
 </head>
 <body>
-  <div class="space"></div>
   <div class="category-body">
     <div class="categories-wrapper">
       <div class="head-category" onmouseover="showCategories(0)">
@@ -90,60 +69,46 @@ try {
           <div class="head-delimiter"></div>       
         </div>
       </div>
-      <div class="head-category">
+      <div class="head-category" onmouseover="showCategories(1)">
         <div class="img-container">
           <img style="width:22px;max-height:30px" src="https://kontakt.az/wp-content/uploads/2022/10/Saat.png">
         </div>
         <div class="img-name">
           <div class="name">Smart qadjetlər</div>       
-          <div class="head-delimiter"></div>   
-        </div>
-      </div>
-      <div class="head-category">
-        <div class="img-container">
-          <img style="width:22px;max-height:30px" src="https://kontakt.az/wp-content/uploads/2022/10/Komputer.png">
-        </div>
-        <div class="img-name">
-          <div class="name">Notbuklar, PK, planşetlər</div>
-          <div class="head-delimiter"></div>
         </div>
       </div>
     </div>
     <div style="display: block;" class="categories-tree">
-        <div class="categories">
-          <?php categoryRender($smartPhoneCategories, 0) ?>
-        </div>
-    </div>
-    <?php die(); ?>
-    <div style="display: none;" class="categories-tree">
-      <?php foreach($smartGadjets as $mainCategory) {   
-        if ($mainCategory['nesting_type'] === 0) {  
-      ?>
-      <div class="category-block">
-          <div class="main-category"><?= $mainCategory['name'] ?></div>  
-          <div class="sub-categories">
-            <?php subCategoryRender($smartGadjets, $mainCategory, 15);?>
-          </div>  
+      <div class="categories">
+        <?php for ($index = 0; $index < count($smartPhoneCategories); $index++) {
+          $category = $smartPhoneCategories[$index];
+          if (empty($category['parent_id'])) {
+        ?>
+        <a href="products.php?category_id=<?= $category['id'] ?>&product_type=<?= $category['product_type'] ?>">
+          <div class="category"><?= $category['name'] ?></div>
+        </a>
+        <?php 
+            categoryRender($smartPhoneCategories, $category, 15);
+          }
+        }
+        ?>
       </div>
-      <?php
-          } 
-        } 
-      ?>
     </div>
     <div style="display: none;" class="categories-tree">
-      <?php foreach($infiniteNestedArrays as $mainCategory) {   
-        if ($mainCategory['nesting_type'] === 0) {  
-      ?>
-      <div class="category-block">
-          <div class="main-category"><?= $mainCategory['name'] ?></div>  
-          <div class="sub-categories">
-            <?php subCategoryRender($infiniteNestedArrays, $mainCategory, 15);?>
-          </div>  
+      <div class="categories">
+        <?php for ($index = 0; $index < count($smartGadjets); $index++) {
+          $category = $smartGadjets[$index];
+          if (empty($category['parent_id'])) {
+        ?>
+        <a href="products.php?category_id=<?= $category['id'] ?>&product_type=<?= $category['product_type'] ?>">
+          <div class="category"><?= $category['name'] ?></div>
+        </a>
+        <?php 
+          categoryRender($smartGadjets, $category, 15);
+        }
+        }
+        ?>
       </div>
-      <?php
-          } 
-        } 
-      ?>
     </div>
   </div>
   <script src="main.js"></script>
